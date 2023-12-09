0cca1d018e3c590f8478dc99914fb71550d36217
Jufarkas <139999007+Jufarkas@users.noreply.github.com>
2023-12-09 13:38:51 -0800

    add function to randomize computer choice

---
+++ external.js
@@ -0,0 +1,7 @@
+let answers = ["Rock", "Paper", "Scissors"];
+let computerSelection = answers[Math.floor(Math.random() * answers.length)];
+let x = computerSelection
+
+function getComputerChoice () {
+    return answers[Math.floor(Math.random() * answers.length)];
+};
