33fc0b8a8685259a48d052dbec51430978d9c191
Jufarkas <139999007+Jufarkas@users.noreply.github.com>
2023-12-10 12:13:17 -0800

    add function to advise of outcome between players

--- external.js
+++ external.js
@@ -1,7 +1,32 @@
-let answers = ["Rock", "Paper", "Scissors"];
-let computerSelection = answers[Math.floor(Math.random() * answers.length)];
-let x = computerSelection
+const answers = ["Rock", "Paper", "Scissors"];
+const result = ["Paper beats Rock", "Rock beats Scissors", "Scissors beats Paper", "It's a Tie!"];
 
-function getComputerChoice () {
-    return answers[Math.floor(Math.random() * answers.length)];
+
+function getComputerChoice() {
+    return answers[Math.floor(Math.random() * answers.length)] + " is the computer's choice";
 };
+
+
+function getPlayerChoice() {
+    return answers[Math.floor(Math.random() * answers.length)] + " is the player's choice";
+};
+
+
+
+function startRound (playerSelection, computerSelection) {
+    if (playerSelection == "Rock" && computerSelection == "Paper") {
+        return result[0] + ", computer wins!";
+    } else if  (playerSelection == "Paper" && computerSelection == "Rock") {
+        return result[0] + ", player wins!";
+    } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        return result[1] + ", computer wins!";
+    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        return result[1] + ", player wins!";
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        return result[2] + ", computer wins!";
+    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        return result[2] + ", player wins!";
+    } else if (playerSelection == "Rock" && computerSelection == "Rock" || playerSelection == "Paper" && computerSelection == "Paper" || playerSelection == "Scissors" && computerSelection == "Scissors") {
+        return result[3];
+    }
+}
